39c7cc4e863d1b59a28dd0ca70f89b4556b1808f
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-03 17:15:32 +0600

    added a route at /chefs for all chefs data from the json file. Hitting this route gives all chefs data in json

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Chef's diary server is running...");
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 });
 
 app.listen(port, () => {
