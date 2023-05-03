a2f60ca3ca70ba3b70d9f08224d08e8a4ee4f701
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-03 08:02:33 +0600

    added cors middleware to resolve cors relatated issues

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Chef's diary server is running...");
